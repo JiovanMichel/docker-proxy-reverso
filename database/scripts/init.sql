257974f6bf9251a41a371e2fcb6d853406035cad
Jiovan <jiiovan@unochapeco.edu.br>
2022-09-26 22:50:56 -0300

    chore: Remove comments

--- database/scripts/init.sql
+++ database/scripts/init.sql
@@ -7,5 +7,4 @@ CREATE TABLE IF NOT EXISTS people (
     name varchar(255)
 );	
 
--- INSERT INTO people(name) VALUES ('Jiovan Michel da Silva');
 INSERT IGNORE INTO people (id, name) VALUES (1, 'Jiovan Michel da Silva');
